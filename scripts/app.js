105689767ca613686853ce430e0102a591a8c625
roxinfi <v_patel233507@fanshaweonline.ca>
2025-06-19 18:38:57 -0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,20 @@
-// Get the input element
 const numberInput = document.querySelector("#numberInput");
 
-// Fruit object
+
 const fruit = {
     fruitName: "Apple",
     fruitColor: "Red",
     fruitTaste: "Sweet"
 };
 
-// Get display elements
 const nameEl = document.querySelector("#Name");
 const colorEl = document.querySelector("#Color");
 const tasteEl = document.querySelector("#Taste");
 
-// Display fruit properties using template literals
 nameEl.innerHTML = `Name: ${fruit.fruitName}`;
 colorEl.innerHTML = `Color: ${fruit.fruitColor}`;
 tasteEl.innerHTML = `Taste: ${fruit.fruitTaste}`;
 
-// Listen for input change
 numberInput.addEventListener("input", () => {
     const range = numberInput.value;
 
